82fd1ff864d4ee9658fa1c8cde815e083720a224
KHirgeto <kumlachew12292000@gmail.com>
2020-07-04 22:56:17 -0400

    Today i have worked ont he scroll feature int he registration actvity and programed the activity to change color when the edit text is clicked. However i need to do more work on it and make it function better.

--- app/src/main/java/com/kingstech_app/no1_resturant_app/RegisterActivity.java
+++ app/src/main/java/com/kingstech_app/no1_resturant_app/RegisterActivity.java
@@ -3,12 +3,52 @@ package com.kingstech_app.no1_resturant_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText emailET, nameET, passwordET;
+    Button signUpBTN;
+    ImageButton fbBTN, ggBTN, ttBTN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        emailET = (EditText) findViewById(R.id.emailET);
+        nameET = (EditText) findViewById(R.id.nameET);
+        passwordET = (EditText) findViewById(R.id.passwordET);
+        signUpBTN = (Button) findViewById(R.id.signupBTN);
+        fbBTN = (ImageButton) findViewById(R.id.fbBTN);
+        ggBTN = (ImageButton) findViewById(R.id.ggBTN);
+        ttBTN = (ImageButton) findViewById(R.id.ttBTN);
+
+        emailET.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nameET.setBackgroundResource(R.drawable.edit_text_one);
+                passwordET.setBackgroundResource(R.drawable.edit_text_one);
+                emailET.setBackgroundResource(R.drawable.edittexttwo);
+            }
+        });
+        nameET.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                emailET.setBackgroundResource(R.drawable.edit_text_one);
+                passwordET.setBackgroundResource(R.drawable.edit_text_one);
+                nameET.setBackgroundResource(R.drawable.edittexttwo);
+            }
+        });
+        passwordET.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                emailET.setBackgroundResource(R.drawable.edit_text_one);
+                nameET.setBackgroundResource(R.drawable.edit_text_one);
+                passwordET.setBackgroundResource(R.drawable.edittexttwo);
+            }
+        });
     }
 }
