063b1e96fcb8064541f9184882ad6c82c5fb7a3d
KHirgeto <kumlachew12292000@gmail.com>
2020-07-07 20:14:41 -0400

    This is the latest version

---
+++ app/src/main/java/com/kingstech_app/no1_resturant_app/LogInActivity.java
@@ -0,0 +1,13 @@
+package com.kingstech_app.no1_resturant_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
--- app/src/main/java/com/kingstech_app/no1_resturant_app/RegisterActivity.java
+++ app/src/main/java/com/kingstech_app/no1_resturant_app/RegisterActivity.java
@@ -3,16 +3,20 @@ package com.kingstech_app.no1_resturant_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ScrollView;
 
 public class RegisterActivity extends AppCompatActivity {
 
     EditText emailET, nameET, passwordET;
     Button signUpBTN;
     ImageButton fbBTN, ggBTN, ttBTN;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,29 +30,37 @@ public class RegisterActivity extends AppCompatActivity {
         ggBTN = (ImageButton) findViewById(R.id.ggBTN);
         ttBTN = (ImageButton) findViewById(R.id.ttBTN);
 
-        emailET.setOnClickListener(new View.OnClickListener() {
+
+
+        emailET.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                nameET.setBackgroundResource(R.drawable.edit_text_one);
+            public boolean onTouch(View v, MotionEvent event) {
                 passwordET.setBackgroundResource(R.drawable.edit_text_one);
+                nameET.setBackgroundResource(R.drawable.edit_text_one);
                 emailET.setBackgroundResource(R.drawable.edittexttwo);
+                return false;
             }
         });
-        nameET.setOnClickListener(new View.OnClickListener() {
+        nameET.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
+            public boolean onTouch(View v, MotionEvent event) {
                 emailET.setBackgroundResource(R.drawable.edit_text_one);
                 passwordET.setBackgroundResource(R.drawable.edit_text_one);
                 nameET.setBackgroundResource(R.drawable.edittexttwo);
+                return false;
             }
         });
-        passwordET.setOnClickListener(new View.OnClickListener() {
+        passwordET.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
+            public boolean onTouch(View v, MotionEvent event) {
                 emailET.setBackgroundResource(R.drawable.edit_text_one);
                 nameET.setBackgroundResource(R.drawable.edit_text_one);
                 passwordET.setBackgroundResource(R.drawable.edittexttwo);
+                return false;
             }
         });
+
+      
     }
+
 }
